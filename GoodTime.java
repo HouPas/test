12ee87c0c1316d90b6209c7bc06f65d5d074e65a
HM <herve.martinez@lecnam.net>
2022-01-13 19:03:43 +0100

    =essai commit distant

---
+++ GoodTime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class GoodTime {
+
+    public static String SaisirChaine() {
+
+        return new Scanner(System.in).nextLine();
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println("Good Time");
+    }
+}
